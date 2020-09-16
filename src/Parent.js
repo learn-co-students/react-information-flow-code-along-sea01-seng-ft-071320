1afb3d0f79d41b3e636ea4d86f19d4fc8f140cca
bdb2381 <brad.beggs@gmail.com>
2020-09-15 21:18:21 -0700

    Done.

--- src/Parent.js
+++ src/Parent.js
@@ -1,21 +1,30 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { getRandomColor } from './randomColorGenerator.js'
 import Child from './Child'
 
-class Parent extends Component {
+class Parent extends React.Component {
 
   constructor() {
     super()
     this.state = {
-      color: getRandomColor()
+      color: getRandomColor(),
+      childrenColor: `#FFF`
     }
   }
+
+  changeColor = (newChildColor) => {
+    this.setState( {
+      color: getRandomColor(),
+      childrenColor: newChildColor
+    })
+
+  } // end changeColor
 
   render() {
     return (
       <div className="parent" style={{backgroundColor: this.state.color}}>
-        <Child />
-        <Child />
+        <Child color={this.state.childrenColor} handleColorChange={this.changeColor}/>
+        <Child color={this.state.childrenColor} handleColorChange={this.changeColor}/>
       </div>
     )
   }
